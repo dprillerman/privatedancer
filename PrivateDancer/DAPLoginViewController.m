496d3bbbc9e07befff5300a48a43a4c11dfeb0a9
De Haven Prillerman <dprillerman@gmail.com>
2013-08-28 17:59:06 -0400

    Connected login view controllers

--- PrivateDancer/DAPLoginViewController.m
+++ PrivateDancer/DAPLoginViewController.m
@@ -28,13 +28,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    // Release any retained subviews of the main view.
+    self.userTextField = nil;
+    self.passwordTextField = nil;
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark IB Actions
+- (IBAction)logInPressed:(id)sender {
+    [PFUser logInWithUsernameInBackground:self.userTextField.text password:self.passwordTextField.text block:^(PFUser *user, NSError *error) {
+        if (user) {
+            //Open the wall
+            // PFGeoPoint *point = [PFGeoPoint geoPointWithLatitude:40.0 longitude:-30.0];
+            //[user setObject:point forKey:@"location"];
+            [self performSegueWithIdentifier:@"LoginSuccessful" sender:self];
+        } else {
+            //Something bad has ocurred
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [errorAlertView show];
+        }
+    }];
 }
 
 @end
